dadc2b382a49ff3ceb99966dc8e4250368ade6e2
Bruce Steedman <bruce.steedman@gmail.com>
2013-12-18 14:01:28 +0000

    Tried different way to lookup heroku DATABASE_URL env

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,8 +1,9 @@
 env = ENV['RACK_ENV'] || 'development'
+heroku_db = `heroku config:get DATABASE_URL`
 
 # setup DB depending on env we want
 # DataMapper::Logger.new($stdout, :debug) # enables debug mode - puts SQL on command line - BEFORE SETUP
-connection_string = ENV[`heroku config:get DATABASE_URL`] || "postgres://localhost/bookmark_manager_#{env}"
+connection_string = ENV[heroku_db] || "postgres://localhost/bookmark_manager_#{env}"
 DataMapper.setup(:default, connection_string)
 
 #databaase address format as follows: dbtype://user:password@hostname:port/databasename
